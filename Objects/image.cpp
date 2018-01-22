10254f333f513f461273ce37971e6a0021cfb657
OtterBoops <me44888@gmail.com>
2018-01-22 18:18:16 +0200

    Image Class Changes: -Moved Vector operations to getUserInput() -Completed documentation for .h and .cpp files

--- Objects/image.cpp
+++ Objects/image.cpp
@@ -1,54 +1,68 @@
 #include "image.h"
 #include "ui_image.h"
 
-
+/*Initialize the class. Takes an extra parameter 'tag' which is passed
+by MainWindow to store HTML tag type */
 Image::Image(QWidget *parent, QString tag) :
     QDialog(parent),
     ui(new Ui::Image)
 {
     ui->setupUi(this);
-    element = tag;
+    element = tag; //Local QString 'element' set to the 'tag' passed.
 }
 
+//Destructor
 Image::~Image()
 {
     delete ui;
 }
+
+//Function to store user input in local variables.
 void Image::getUserInput()
 {
-    address = ui->address->text();
-    height = ui->height->text();
-    width = ui->width->text();
+    address = ui->address->text(); //Sets 'address' to the user input from UI field 'address'
+    height = ui->height->text(); //Sets 'height' to the user input from UI field 'height'
+    width = ui->width->text(); //Sets 'width' to the user input from UI field 'width'
 
+    //If user input from height field is 0, sets height to auto
     if(height == "0")
         height = "auto";
 
+    //If user input from width field is 0, sets width to auto
     if(width == "0")
         width = "auto";
-}
 
-void Image::on_OkButton_clicked()
-{
-    Image::getUserInput();
+
+    //Appends the tag variables into a QVector of QStrings
     storage.append("<" + element);
     storage.append(" src=\""+ address + "\" ");
     storage.append("height=\"" + height + "\" ");
-    storage.append("width=\"" + width + "\"");
-    storage.append(">");
-    this->close();
+    storage.append("width=\"" + width + "\">");
 }
 
+//OK Button Action
+void Image::on_OkButton_clicked()
+{
+    getUserInput(); //Processes user input
+    this->close(); //Closes window
+}
+
+//Cancel Button Action
 void Image::on_CancelButton_clicked()
 {
-    this->close();
+    this->close(); //Just closes the window.
 }
 
+/* Function to get all of the elements from the 'storage'
+ * vector and merge them into a single string.
+ * Returns a QString
+*/
 QString Image::getFullString()
 {
-    for (int i = 0; i < storage.size(); i++)
+    for (int i = 0; i < storage.size(); i++) //Iterates through the QVector elements.
     {
-        fullString.append(storage[i]);
+        fullString.append(storage[i]); //Append each element to the string
 
     }
-    return fullString;
+    return fullString; //Return the complete string which will represent the element.
 }
--- Objects/image.h
+++ Objects/image.h
@@ -12,20 +12,20 @@ class Image : public QDialog
     Q_OBJECT
 
 public:
-    explicit Image(QWidget *parent = 0, QString tag = "");
-    ~Image();
-    QString getFullString();
+    explicit Image(QWidget *parent = 0, QString tag = ""); //Constructor, takes an extra parameter 'tag'
+    ~Image(); //Destructor
+    QString getFullString(); //Getter for full HTML tag with user input.
 
 private slots:
-    void on_OkButton_clicked();
-    void on_CancelButton_clicked();
+    void on_OkButton_clicked(); //Private slot for OK Button
+    void on_CancelButton_clicked(); //Private slot for Cancel Button
 
 private:
-    Ui::Image *ui;
-    void getUserInput();
+    Ui::Image *ui; //UI for the user input
+    void getUserInput(); //Function to save the user input in QStrings.
 
-    QVector<QString> storage;
-    QString element, address, height, width, fullString;
+    QVector<QString> storage; //QVector of QStrings for storing all of the user variables in seperate elements
+    QString element, address, height, width, fullString; //QStrings needed for tag processing
 };
 
 #endif // IMAGE_H
