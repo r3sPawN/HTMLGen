f4ffc9e950ab5ad10103f0d8080bfc06ce13b08d
OtterBoops <me44888@gmail.com>
2018-01-22 19:03:53 +0200

    Image Class Changes:
    -Moved Vector operations to getUserInput()
    -Completed documentation for .h and .cpp files

--- Objects/link.cpp
+++ Objects/link.cpp
@@ -1,46 +1,59 @@
 #include "link.h"
 #include "ui_link.h"
 
+/*Initialize the class. Takes an extra parameter 'tag' which is passed
+by MainWindow to store HTML tag type */
 Link::Link(QWidget *parent, QString tag) :
     QDialog(parent),
     ui(new Ui::Link)
 {
     ui->setupUi(this);
-    element = tag;
+    element = tag; //Local QString 'element' set to the 'tag' passed.
 }
 
+//Destructor
 Link::~Link()
 {
     delete ui;
 }
 
+//Function to store user input in local variables.
 void Link::getUserInput()
 {
-    address = ui->address->text();
-    userText = ui->PlainText->toPlainText();
-}
+    address = ui->address->text(); //Sets 'address' to the user input from UI field 'address'
+    userText = ui->PlainText->toPlainText(); //Sets 'userText' to the user input from UI field 'PlainText'
 
-void Link::on_OkButton_clicked()
-{
-    Link::getUserInput();
+
+    //Appends the tag variables into a QVector of QStrings which represents a valid HTML tag
     storage.append("<" + element);
     storage.append(" href=\"" + address + "\">");
     storage.append(userText);
     storage.append("</" + element + ">");
-    this->close();
 }
 
+//OK Button Action
+void Link::on_OkButton_clicked()
+{
+    getUserInput(); //Processes user input
+    this->close(); //Closes window
+}
+
+//Cancel Button Action
 void Link::on_CancelButton_clicked()
 {
-    this->close();
+    this->close(); //Just closes the window.
 }
 
+/* Function to get all of the elements from the 'storage'
+ * vector and merge them into a single string.
+ * Returns a QString
+*/
 QString Link::getFullString()
 {
-    for (int i = 0; i < storage.size(); i++)
+    for (int i = 0; i < storage.size(); i++) //Iterates through the QVector elements.
     {
-        fullString.append(storage[i]);
-
+        fullString.append(storage[i]); //Append each element to the string
     }
-    return fullString;
+
+    return fullString; //Return the complete string which will represent the element.
 }
--- Objects/link.h
+++ Objects/link.h
@@ -13,23 +13,21 @@ class Link : public QDialog
     Q_OBJECT
 
 public:
-    explicit Link(QWidget *parent = 0, QString tag = "");
-    ~Link();
+    explicit Link(QWidget *parent = 0, QString tag = ""); //Constructor, takes an extra parameter 'tag'
+    ~Link(); //Destructor
 
-    QString getFullString();
+    QString getFullString(); //Getter for full HTML tag with user input.
 
 private slots:
-    void on_OkButton_clicked();
-
-    void on_CancelButton_clicked();
+    void on_OkButton_clicked(); //Private slot for OK Button
+    void on_CancelButton_clicked(); //Private slot for Cancel Button
 
 private:
-    Ui::Link *ui;
+    Ui::Link *ui; //UI for the user input
+    void getUserInput(); //Function to save the user input in QStrings.
 
-    QVector<QString> storage;
-    QString element, address, userText, fullString;
-
-    void getUserInput();
+    QVector<QString> storage; //QVector of QStrings for storing all of the user variables in seperate elements
+    QString element, address, userText, fullString; //QStrings needed for tag processing
 };
 
 #endif // LINK_H
