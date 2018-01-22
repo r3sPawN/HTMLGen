8d6f74642f79c8655e1ebc050ae2612042e773f8
OtterBoops <me44888@gmail.com>
2018-01-22 20:26:01 +0200

    Text Class Changes:
    -Moved Vector operations to getUserInput()
    -Completed documentation for .h and .cpp files

--- Objects/text.cpp
+++ Objects/text.cpp
@@ -1,56 +1,64 @@
 #include "text.h"
 #include "ui_text.h"
 
+/*Initialize the class. Takes an extra parameter 'tag' which is passed
+by MainWindow to store HTML tag type */
 Text::Text(QWidget *parent, QString tag) :
     QDialog(parent),
     ui(new Ui::Text)
 {
     ui->setupUi(this);
-    element = tag;
+    element = tag; //Local QString 'element' set to the 'tag' passed.
 }
 
+//Destructor
 Text::~Text()
 {
-    qDebug() << "Destroy" << endl;
     delete ui;
 }
 
+//Function to store user input in local variables.
 void Text::getUserInput()
 {
-    color = ui->colorBox->text();
-    fontface = ui->fontFaceBox->text();
-    align = ui->alignBox->currentText();
-    usertext = ui->PlainText->toPlainText();
-}
+    color = ui->colorBox->text(); //Sets 'color' to the user input from UI field 'colorBox'
+    fontface = ui->fontFaceBox->text(); //Sets 'fontface' to the user input from UI field 'fontFaceBox'
+    align = ui->alignBox->currentText(); //Sets 'align' to the user input from UI field 'alignBox'
+    usertext = ui->PlainText->toPlainText(); //Sets 'usertext' to the user input from UI field 'PlainText'
 
-void Text::on_OkButton_clicked()
-{
-    Text::getUserInput();
-    storage.append("<");
-    storage.append(element);
-    storage.append(" style=\"");
+    //Appends the tag variables into a QVector of QStrings which represents a valid HTML tag
+    storage.append("<" + element + " style=\"");
     storage.append("color: " + color + "; ");
     storage.append("font-family: " + fontface + "; ");
-    storage.append("text-align: " + align + ";\"");
-    storage.append(">");
+    storage.append("text-align: " + align + ";\">");
     storage.append(usertext);
     storage.append("</" + element + ">");
-    this->close();
 }
 
+//OK Button Action
+void Text::on_OkButton_clicked()
+{
+    Text::getUserInput(); //Processes user input
+    this->close(); //Closes window
+}
+
+//Cancel Button Action
 void Text::on_CancelButton_clicked()
 {
-   this->close();
+   this->close(); //Just closes the window.
 }
 
+/* Function to get all of the elements from the 'storage'
+ * vector and merge them into a single string.
+ * Returns a QString
+*/
 QString Text::getFullString()
 {
-    for (int i = 0; i < storage.size(); i++)
+    for (int i = 0; i < storage.size(); i++) //Iterates through the QVector elements.
     {
-        fullString.append(storage[i]);
+        fullString.append(storage[i]); //Append each element to the string
 
     }
-    return fullString;
+    return fullString; //Return the complete string which will represent the element.
 }
 
 
--- Objects/text.h
+++ Objects/text.h
@@ -14,21 +14,21 @@ class Text : public QDialog
     Q_OBJECT
 
 public:
-    explicit Text(QWidget *parent = 0, QString tag = "");
-    ~Text();
+    explicit Text(QWidget *parent = 0, QString tag = ""); //Constructor, takes an extra parameter 'tag'
+    ~Text(); //Destructor
 
-    QString getFullString();
+    QString getFullString(); //Getter for full HTML tag with user input.
 
 private slots:
-    void on_OkButton_clicked();
-    void on_CancelButton_clicked();
+    void on_OkButton_clicked(); //Private slot for OK Button
+    void on_CancelButton_clicked(); //Private slot for Cancel Button
 
 private:
-    Ui::Text *ui;
-    void getUserInput();
+    Ui::Text *ui; //UI for the user input
+    void getUserInput(); //Function to save the user input in QStrings.
 
-    QVector<QString> storage;
-    QString element, color, align, fontface, usertext, fullString;
+    QVector<QString> storage; //QVector of QStrings for storing all of the user variables in seperate elements
+    QString element, color, align, fontface, usertext, fullString; //QStrings needed for tag processing
 };
 
 #endif // TEXT_H
